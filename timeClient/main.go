78c898fda058bb9120822c400501fb4ae0fad8e2
wkd3475 <wkd3475@gmail.com>
2020-07-14 13:03:11 +0900

    init

---
+++ timeClient/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/url"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+var (
+	total_o int64
+)
+
+func main() {
+	total_o = 0
+	// u := url.URL{Scheme: "ws", Host: "localhost:9090", Path: "/time"}
+	u := url.URL{Scheme: "ws", Host: "192.168.80.148:9090", Path: "/time"}
+	fmt.Printf("connecting to %s\n", u.String())
+
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+
+	done := make(chan struct{})
+
+	go func() {
+		defer close(done)
+		for {
+			_, message, err := c.ReadMessage()
+			t4 := time.Now().UnixNano() / 1e6
+			if err != nil {
+				fmt.Println("read:", err)
+				return
+			}
+			s := strings.Split(string(message), ",")
+			t1, _ := strconv.ParseInt(s[1], 10, 64)
+			t2, _ := strconv.ParseInt(s[2], 10, 64)
+			t3, _ := strconv.ParseInt(s[3], 10, 64)
+
+			o := (t2 + t3 - t1 - t4) / 2
+			total_o += o
+		}
+	}()
+
+	for i := 1; i <= 100; i++ {
+		time.Sleep(time.Millisecond * 500)
+		datas := []byte(strconv.Itoa(i) + "," + strconv.FormatInt(time.Now().UnixNano()/1e6, 10))
+		err := c.WriteMessage(websocket.TextMessage, datas)
+		fmt.Println(string(datas))
+		if err != nil {
+			fmt.Println("write:", err)
+			return
+		}
+	}
+	time.Sleep(time.Millisecond * 1000)
+
+	fmt.Println(total_o / 100)
+}
---
+++ timeServer/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	ws "github.com/gorilla/websocket"
+)
+
+var (
+	timeseverConnCtx = context.Background()
+)
+
+var upgrader = ws.Upgrader{
+	CheckOrigin: func(_ *http.Request) (ok bool) {
+		ok = true
+		return
+	},
+}
+
+func timeHandler(w http.ResponseWriter, r *http.Request) {
+	var (
+		err    error
+		c      *ws.Conn
+		cancel context.CancelFunc
+	)
+
+	timeseverConnCtx, cancel = context.WithCancel(context.Background())
+	defer cancel()
+
+	if c, err = upgrader.Upgrade(w, r, nil); err != nil {
+		log.Println("failed to upgrade", err)
+		return
+	}
+	defer c.Close()
+
+	for {
+		// receive data
+		_, data, err := c.ReadMessage()
+		t2 := strconv.FormatInt(time.Now().UnixNano()/1e6, 10)
+		s := strings.Split(string(data), ",")
+		t1 := string(s[1])
+		if err != nil {
+			log.Println("failed to read")
+			break
+		}
+		datas := []byte(s[0] + "," + t1 + "," + t2 + "," + strconv.FormatInt(time.Now().UnixNano()/1e6, 10))
+		if err := c.WriteMessage(ws.TextMessage, datas); err != nil {
+			log.Println("failed to write")
+			break
+		}
+		fmt.Println(string(datas))
+	}
+
+	cancel()
+}
+
+func setupRoutes() {
+	http.HandleFunc("/time", timeHandler)
+	if err := http.ListenAndServe(":9090", nil); err != nil {
+		log.Println("failed to listend and serve")
+	}
+}
+
+func main() {
+	setupRoutes()
+}
